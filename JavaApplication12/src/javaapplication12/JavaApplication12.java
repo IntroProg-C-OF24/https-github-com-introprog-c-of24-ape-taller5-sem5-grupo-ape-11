179c6725cd8c737e6aee775015bbfeae96434ff5
Flor María <fmvaldevieso@utpl.edu.ec>
2023-11-13 13:09:08 -0500

    Edad´para votar

---
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,16 @@
+package javaapplication12;
+
+/**
+ *
+ * @author D E L L
+ */
+public class JavaApplication12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
